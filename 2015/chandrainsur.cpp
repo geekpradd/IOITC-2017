96a4f824470bffa115240f199ce976ce741ee8b0
geekpradd <pradd@outlook.com>
2017-03-05 21:07:34 +0530

    Solve Chandra and Insurgents

--- 2015/chandrainsur.cpp
+++ 2015/chandrainsur.cpp
@@ -1,49 +1,136 @@
 #include <bits/stdc++.h>
+#include <cstdio>
+#define ii pair<int, int>
 using namespace std;
+ii *tree;
 
-// The Scientist... 
+// First segtree with lazy propagation
+// P.S: I got new spectacles :D
+// FMM coming up
+// Last time study for boards..
+// Hope not to screw up IOITC!
 
-// This is naive O(n^2). 
-// Other file has O(NlogN) segtree approach
+// Complexity: O(Q log N)
+int init(int i, int l, int r){
+	tree[i].first = 0;
+	
+	int mid = (r-l)/2;
+	if (r==l){
 
+		tree[i].second = 1;
+	}
+	else {
+		tree[i].second = init(2*i, l, l+mid) + init(2*i+1, l+mid+1, r);
+	}
+	return tree[i].second;
+}
+
+int query(int i, int l, int r, int ql, int qr){
+	int mid = (r-l)/2;
+	if (r < ql || l > qr){
+		// lies outside
+		return 0;
+	}
+	else if (l >= ql && r <= qr){
+		// lies completely inside
+		return tree[i].second;
+	}
+	else {
+		if (tree[i].second == 0){
+			return 0;
+		}
+		else {
+			return query(2*i, l, l+mid, ql, qr) + query(2*i+1, l+mid+1, r, ql, qr);
+		}
+	}
+
+}
+
+int paint(int i, int l, int r, int ql, int qr){
+	if (r < ql || l > qr){
+		// lies outside
+	}
+	else if (l >= ql && r <= qr){
+		// lies completely inside
+		tree[i].first++;
+		tree[i].second = 0;
+	}
+	else {
+		// partial overlap
+		int mid = (r-l)/2;
+		tree[i].second = paint(2*i, l, l+mid, ql, qr) + paint(2*i+1, l+mid+1, r, ql, qr);
+		if (tree[i].first != 0){
+			tree[i].second = 0;
+		}
+	}
+
+	return tree[i].second;
+}
+int coat(int i, int l, int r, int ql, int qr){
+	int mid = (r-l)/2;
+	if (r < ql || l > qr){
+		// lies outside
+	}
+	else if (l >= ql && r <= qr){
+		// lies completely inside
+		tree[i].first--;
+		if (tree[i].first == 0){
+			if (l != r){
+				tree[i].second = tree[2*i].second + tree[2*i+1].second;
+			}
+			else {
+				tree[i].second = 1;
+			}
+		}
+	}
+	else {
+		// partial overlap
+		tree[i].second = coat(2*i, l, l+mid, ql, qr) + coat(2*i+1, l+mid+1, r, ql, qr);
+		if (tree[i].first != 0){
+			tree[i].second = 0;
+		}
+	}
+	return tree[i].second;
+}
 signed main(){
 	int n, m, q;
-	cin >> n >> m >> q;
-	int arr[n];
-	for (int i=1; i<n; ++i) arr[i] = 0;
+	scanf("%d %d %d", &n, &m, &q);
+	// The graph has n nodes
+	// and (n-1) initial edges which are stored in the seg_tree
+	tree = new ii[3*n+1];
+	init(1, 1, n-1);
 
-	for (int i=0; i<m; ++i){
+	while (m--){
 		int a, b;
-		cin >> a >> b;
+		scanf("%d %d", &a, &b);
 		if (a > b) swap(a, b);
-		for (int j=a; j<b; ++j) arr[j]++;
+		// Our segtree indices are from a to b-1
+		paint(1, 1, n-1, a, b-1);
 	}
-	
 	while (q--){
 		int code;
-		cin >> code;
+		scanf("%d", &code);
 		if (code == 0){
 			int a, b;
-			cin >> a >> b;
+			scanf("%d %d", &a, &b);
 			if (a > b) swap(a, b);
-			for (int j=a; j<b; ++j) arr[j]++;
+			paint(1, 1, n-1, a, b-1);
 		}
 		if (code == 1){
 			int a, b;
-			cin >> a >> b;
+			scanf("%d %d", &a, &b);
 			if (a > b) swap(a, b);
-			for (int j=a; j<b; ++j) arr[j]--;
+			coat(1, 1, n-1, a, b-1);
 		}
 		if (code == 2){
 			int a, b;
-			cin >> a >> b;
+			scanf("%d %d", &a, &b);
 			if (a > b) swap(a, b);
-			int count = 0;
-			for (int j=a; j<b; ++j){
-				if (arr[j] == 0) count++;
-			}
-			cout << count << endl;
+
+			int count = query(1, 1, n-1, a, b-1);
+			printf("%d\n", count);
 		}
 	}
+
 	return 0;
 }
