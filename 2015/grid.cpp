03e4ccebefa6a5ce8ad1e5416984248309d6f33e
geekpradd <pradd@outlook.com>
2017-04-15 15:52:07 +0530

    Changes

--- 2015/grid.cpp
+++ 2015/grid.cpp
@@ -1,168 +1,76 @@
 #include <bits/stdc++.h>
-#include <cstdio>
-#include <cmath>
-#define vi vector<int>
-struct interval {
-	int l;
-	int r;
-	int d;
-	int u;
-	// int id;
-};
+#define ii pair<int, int>
+#define NM 200000
 using namespace std;
-int block;
-
-bool comp(const interval &a, const interval &b){
-	if (a.l/block != b.l/ block){
-		return a.l/block < b.l/block;
+ii def = ii(-1, -1);
+inline int mid(int l, int r){
+	return (r-l)/2;
+}
+ii seg_tree[4*NM];
+void build(int i, int l, int r){
+	seg_tree[i] = ii(NM + 5, 0);
+	if (l != r){
+		build(i + i, l, l + mid(l, r));
+		build(i + i + 1, l + mid(l, r) + 1, r);
 	}
-	return a.r > b.r; // modified mo order, still N(SQRT(N))
+}
+void update(int i, int l, int r, int ql, int qr, ii car){
+	ii better = ii(min(seg_tree[i].first, car.first), max(seg_tree[i].second, car.second));
+	if (l > qr || r < ql){
+
+	}
+	else if (l >= ql && r <= qr){
+		seg_tree[i] = better;		
+	}
+	else if (l != r){
+		update(i + i, l, l + mid(l, r), ql, qr, better);
+		update(i + i + 1, l + mid(l, r) + 1, r, ql, qr, better);	
+	}
+}
+ii query(int i, int l, int r, int ql, int qr, ii car){
+	ii better = ii(min(seg_tree[i].first, car.first), max(seg_tree[i].second, car.second));
+	if (l > qr || r < ql){
+		return def;
+	}
+	else if (l >= ql && r <= qr){
+		return better;
+	}
+	ii q1 = query(i + i, l, l + mid(l, r), ql, qr, better);
+	ii q2 = query(i + i + 1, l + mid(l, r) + 1, r, ql, qr, better);
+ 	if (q1 == def){
+ 		return q2;
+ 	}
+ 	return q1;
 }
 
 signed main(){
 	int n, u, q;
-	scanf("%d %d %d", &n, &u, &q);
-	block = sqrt(n);
-	// Mo's Algorithm
-	vi grid(n+1, 0); 
-	// grid will serve two purpose:
-	// a) It will show wheter current index has left bracket or right
-	// b) If so then it will show the parent of this left or right bracket
-	interval updates[u+1];
-	interval nout = {0, n, 0, n};
-	updates[0] = nout;
-	// int id_link[u+1];
-
-	for (int i=1; i<=u; ++i){
-		int a, b, c, d;
-		scanf("%d %d %d %d", &a, &b, &c, &d);
-
-		interval cur = {a, c, b, d};
-		updates[i] = cur; 
+	cin >> n >> u >> q;
+	build(1, 1, n);
+	while (u--){
+		int l, r, y1, y2;
+		cin >> l >> y1 >> r >> y2;
+		update(1, 1, n, l, r, ii(y1, y2));
 	}
-	sort(updates+1, updates+u+1, comp);
-
-	int cur_l = 1, cur_r = n, l = 1, r = n, parent = 1;
-
-	for (int i=1; i<=u; ++i){
-		// id_link[updates[i].id] = i;
-		l = updates[i].l;
-		r = updates[i].r;
-
-		while (cur_r < r){
-			cur_r++;
-			if (grid[cur_r] != 0){
-				parent = grid[cur_r];
-			}
+	while (q--){
+		int l, r, y1, y2;
+		cin >> l >> y1 >> r >> y2;
+		ii range = query(1, 1, n, l, r, ii(NM + 5, 0));
+		if (y2 < range.first || y1 > range.second){
+			cout << 0 << endl;
 		}
-		while (cur_l > l){
-			cur_l--;
-			if (grid[cur_l] != 0){
-				parent = grid[cur_l];
-			}
+		else if (y2 <= range.second && y1 >= range.first ){
+			cout << y2 - y1 + 1<< endl;
 		}
-			
-		while (cur_r > r){
-			cur_r--;
-			if (grid[cur_r] != 0){
-				parent = grid[cur_r];
-			}
-
+		else if (y1 >= range.first){
+			cout << range.second - y1 + 1 << endl;
 		}
-		while (cur_l < l){
-			cur_l++;
-			if (grid[cur_l] != 0){
-				parent = grid[cur_l];
-			}
-		}
-		
-		grid[cur_r] = parent;
-		grid[cur_l] = parent;
-		if (parent != 1){
-			parent--;
-			updates[i].d = min(updates[i].d, updates[parent].d);
-			updates[i].u = max(updates[i].u, updates[parent].u);
-		}
-		parent = i + 1;
-		// Parent signal: 1 = parent is unbounded
-		// Else value - 1 gives location in array
-
-	}
-	for (int i=1; i<=n; ++i){
-		if (grid[i] != 0){
-			cout << "Parent[" << i << "] = " << updates[grid[i]-1].l << ", " << updates[grid[i]-1].r << endl;
-		}
-	}
-	interval query[q+1];
-
-	for (int i=1; i<=q; ++i){
-		int a, b, c, d;
-		scanf("%d %d %d %d", &a, &b, &c, &d);
-
-		interval cur = {a, c, b, d};
-		query[i] = cur; 
-		cout << "llf = " << query[i].l << endl;
-		cout << "rrf = " << query[i].r << endl;
-	}
-	sort(query+1, query+q+1, comp);
-
-	cur_l = 1, cur_r = n, parent = 1;
-	for (int i=1; i<=q; ++i){
-		// id_link[updates[i].id] = i;
-		int l = query[i].l;
-		int r = query[i].r;
-		cout << "ll = " << l << endl;
-		cout << "rr = " << r << endl;
-
-
-		while (cur_r < r){
-			cur_r++;
-			if (grid[cur_r] != 0){
-				parent = grid[cur_r];
-				cout << "Changed BAAP to " << parent << endl;
-			}
-		}
-		while (cur_l > l){
-			cur_l--;
-			if (grid[cur_l] != 0){
-				parent = grid[cur_l];
-				cout << "Changed BABP to " << parent << endl;
-			}
-		}
-			
-		while (cur_r > r){
-			cur_r--;
-			if (grid[cur_r] != 0){
-				parent = grid[cur_r];
-				cout << "Changed BACP to " << parent << endl;
-			}
-
-		}
-		while (cur_l < l){
-			cur_l++;
-			if (grid[cur_l] != 0){
-				parent = grid[cur_l];
-				cout << "Changed BADP to " << parent << endl;
-			}
-		}
-		cout << "L = " << cur_l << " R = " << cur_r << endl;
-		if (parent != 1){
-			cout << "Got ID " << parent << endl; 
-			int op = parent - 1;
-			int right = min(query[i].u, updates[op].u);
-			int left = max(query[i].d, updates[op].d);
-			if (right - left > 0){
-				printf("%d\n", (right-left+1)*(r-l+1));
-			}
-			else {
-				printf("0\n");
-			}
+		else if (y2 <= range.second){
+			cout << y2 - range.first + 1 << endl;
 		}
 		else {
-			printf("0\n");
+			cout << range.second - range.first + 1 << endl;
 		}
-
 	}
-
 	return 0;
 }
